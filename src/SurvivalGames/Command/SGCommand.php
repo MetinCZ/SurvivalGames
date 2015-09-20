09fb4ffcc22443ed5ce64ede4af33a614b13a6e7
kvetinac97 <kvetinac97@users.noreply.github.com>
2015-09-20 15:47:25 +0200

    Join Command - Part 1

--- src/SurvivalGames/Command/SGCommand.php
+++ src/SurvivalGames/Command/SGCommand.php
@@ -1,29 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Honza
- * Date: 18. 9. 2015
- * Time: 16:48
- */
+
+// SurvivalGames Command
+// © PocketMineDevelopers 2015
 
 namespace SurvivalGames\Command;
-
 
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use SurvivalGames\SurvivalGames;
+use SurvivalGames\arena\Arena;
 
 class SGCommand extends BaseCommand{
 
-    public function __construct(SurvivalGames $plugin){
-        parent::__construct($plugin, "survivalgames", "Main sg command", "/survivalgames", "/survivalgames", ["sg"]);
+   public $data;
+   public $plugin;
+   public $arena;
+
+    public function __construct(SurvivalGames $plugin, Arena $arena){
+     $this->plugin = $plugin;
+     $this->arena= $arena;
+     $this->data = 
+      [
+       "msg" => $plugin->msg->getAll();
+       "arena" => $plugin->arenas;
+       "players" => $arena->players;
+      ];
+     parent::__construct($plugin, "survivalgames", "Main sg command", "/survivalgames", "/survivalgames", ["sg"]);
     }
 
     public function execute(CommandSender $sender, $alias, array $args) {
-        if($sender instanceof Player){
-            switch($args[0]){
-
-            }
-        }
+     if($sender instanceof Player){
+      switch($args[0]){
+       case "join":
+        
+       break;     
+      }
+     }
     }
 }
